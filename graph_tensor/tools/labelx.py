0701f7d8e1ab2f7a02e22e49d9914610c68fcf43
xinetzone <xinetzone@dingtalk.com>
2020-01-20 13:30:55 +0800

    Optimize the code structure 	modified:   tools/labelx.py

--- graph_tensor/tools/labelx.py
+++ graph_tensor/tools/labelx.py
@@ -13,50 +13,46 @@ class Graph(Drawing):
 
     def _reset_bind(self):
         self._draw_bind()
-        self.current_id = None
+        self.selected_tags = None
 
     def _create_variable(self):
-        '''菜单变量'''
         self.edit_var = StringVar()
+
+    @property
+    def tags_dict(self):
+        return {
+            'Single element': 'current',
+            'All elements': 'all',
+            'All graphic elements': 'graph',
+            '⬜': 'Rectangle',
+            '⚪': 'Oval',
+            '⸺': 'Line',
+            '⯀': 'RectanglePoint',
+            '●': 'OvalPoint'
+        }
 
     def create_menu(self):
         self.master.option_add('*tearOff', False)  # 设定菜单不能移除窗口
         menu_bar = Menu(self.master)  # 创建菜单栏
         self.master['menu'] = menu_bar  # 或者 root.config(menu=menubar)
 
-        options = ('单个元素',
-                   '全部的元素',
-                   '全部的图元素',
-                   '全部 ⬜',
-                   '全部 ⚪',
-                   '全部 ⯀',
-                   '全部 ●',
-                   '全部 ⸺'
-                   )
         # 创建菜单
         edit_bar = Menu(menu_bar)
         move_menu = Menu(edit_bar)
         delete_menu = Menu(edit_bar)
         # 添加级联菜单
-        menu_bar.add_cascade(label="编辑", menu=edit_bar)
-        edit_bar.add_cascade(label="移动", menu=move_menu)
-        edit_bar.add_cascade(label="删除", menu=delete_menu)
+        menu_bar.add_cascade(label="Edit", menu=edit_bar)
+        edit_bar.add_cascade(label="move", menu=move_menu)
+        edit_bar.add_cascade(label="delete", menu=delete_menu)
         kw_menu = {
             'variable': self.edit_var,
             'command': self.bind_graph
         }
-        edit_bar.add_radiobutton(label=f"绘制", **kw_menu)
+        edit_bar.add_radiobutton(label=f"drawing", **kw_menu)
         [move_menu.add_radiobutton(
-            label=f"移动{option}", **kw_menu) for option in options]
+            label=f"move/{option}", **kw_menu) for option in self.tags_dict]
         [delete_menu.add_radiobutton(
-            label=f"删除{option}", **kw_menu) for option in options]
-
-    def select_graph(self, event):
-        '''按压鼠标左键'''
-        self.configure(cursor="target")
-        self.update_xy(event)
-        # 获取当前鼠标指示的对象的 id
-        self.current_id = self.find_withtag('current')
+            label=f"delete/{option}", **kw_menu) for option in self.tags_dict]
 
     def move_strides(self, event):
         x, y = self.x, self.y
@@ -65,94 +61,34 @@ class Graph(Drawing):
         y_move = self.y - y
         return x_move, y_move
 
-    def move_graph(self, event, graph_id_or_tag):
+    def move_graph(self, event):
         x_move, y_move = self.move_strides(event)
-        self.move(graph_id_or_tag, x_move, y_move)
+        self.move(self.selected_tags, x_move, y_move)
 
-    def delete_graph(self, event, graph_id_or_tag):
-        self.delete(graph_id_or_tag)
+    def delete_graph(self, event):
+        self.delete(self.selected_tags)
+
+    def select_graph(self, event, edit_option):
+        self.configure(cursor="target")
+        self.update_xy(event)
+        tags = self.tags_dict[edit_option]
+        if tags == 'current':
+            self.selected_tags = self.find_withtag(tags)
+        else:
+            self.selected_tags = tags
 
     def bind_graph(self):
-        edit = self.edit_var.get()
         self.unbind('<ButtonRelease-1>')
         self.unbind('<1>')
-        if edit == '绘制':
+
+        edit = self.edit_var.get()
+        if edit == 'drawing':
             self._reset_bind()
-        elif '单个元素' in edit:
-            self.bind('<1>', self.select_graph)
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, self.current_id)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(event, 'current')
-            self.bind('<ButtonRelease-1>', action)
-        elif '全部的元素' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'all'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部的图元素' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'graph'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部 ⬜' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'Rectangle'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部 ⚪' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'Oval'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部 ⸺' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'Line'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部 ⯀' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'RectanglePoint'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
-        elif '全部 ●' in edit:
-            self.bind('<1>', self.select_graph)
-            graph_id_or_tag = 'OvalPoint'
-            if '移动' in edit:
-                def action(event): return self.move_graph(
-                    event, graph_id_or_tag)
-            elif '删除' in edit:
-                def action(event): return self.delete_graph(
-                    event, graph_id_or_tag)
-            self.bind('<ButtonRelease-1>',  action)
+        elif 'move' in edit:
+            self.bind('<1>', lambda event: self.select_graph(
+                event, edit.split('/')[1]))
+            self.bind('<ButtonRelease-1>', self.move_graph)
+        elif 'delete' in edit:
+            self.bind('<1>', lambda event: self.select_graph(
+                event, edit.split('/')[1]))
+            self.bind('<ButtonRelease-1>', self.delete_graph)
